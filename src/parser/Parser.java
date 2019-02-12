3ffed96040953b2c864b763270cb87470214a57d
lowjiansheng <lowjiansheng@hotmail.sg>
2019-02-12 15:39:21 +0800

    fix errors with parser

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -5,10 +5,7 @@ import data_structures.Formula;
 import data_structures.Literal;
 import data_structures.Variable;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,32 +15,44 @@ public class Parser {
 
     public static Formula parse(String filePath) {
         File file = new File(filePath);
-        FileInputStream fis = new FileInputStream(file);
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
         InputStreamReader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
         
         String line;
-        // First line: comment line
-        line = br.readLine();
-        // Second line: p
-        line = br.readLine();
-        String[] secondLine = line.split(" ");
-        int numVariables = Integer.parseInt(secondLine[1]);
-        int numClauses = Integer.parseInt(secondLine[2]);
-
-        List<Clause> clauses = new ArrayList<Clause>();
-        for (int i = 0 ; i < numClauses; i++ ) {
+        try {
+            // First line: comment line
             line = br.readLine();
-            try {
-                clauses.addClause(createClause(line));
-            } catch (Exception e){
-                System.out.print(e.getMessage());
-                System.exit(1);
+            // Second line: p
+            line = br.readLine();
+            String[] secondLine = line.split(" ");
+            int numVariables = Integer.parseInt(secondLine[1]);
+            int numClauses = Integer.parseInt(secondLine[2]);
+
+            List<Clause> clauses = new ArrayList<Clause>();
+            for (int i = 0 ; i < numClauses; i++ ) {
+                line = br.readLine();
+                try {
+                    clauses.add(createClause(line));
+                } catch (Exception e){
+                    System.out.print(e.getMessage());
+                    System.exit(1);
+                }
             }
+            Formula form = new Formula(clauses);
+            br.close();
+            return form;
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
         }
-        Formula form = new Formula(clauses);
-        br.close();
-        return form;
+        return null;
     }
 
     private static Clause createClause(String line) throws Exception { 
@@ -65,9 +74,9 @@ public class Parser {
         Variable variable = new Variable(Math.abs(value));
         Literal literal;
         if (value < 0) {
-            literal = new Literal(variable.getId(), true);
+            literal = new Literal(variable, true);
         } else {
-            literal = new Literal(variable.getId(), false);
+            literal = new Literal(variable, false);
         }
         return literal;
     }
