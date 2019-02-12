e6afa4cfaad47e993da62e0a6600a9a3478323d7
lowjiansheng <lowjiansheng@hotmail.sg>
2019-02-12 16:11:09 +0800

    remove commented python code

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,3 @@ class Main {
         Formula form = Parser.parse(filename);
     }
 }
-
-// import parser
-// import algorithms.CDCL.cdcl
-
-// # note: form variable should be in DIMACS CNF format.
-// def main():
-//     form = parser.readFile
-//     form, v = parser.readParameters()
-//     algorithms.CDCL.cdcl.CDCL(form, v)
-
-// if __name__ == "__main__":
-//     main()
