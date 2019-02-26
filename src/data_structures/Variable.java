43c5f045a5dea0038a5a2eef27d8dcd3c4d53c7f
szenius <ting.szeying@gmail.com>
2019-02-26 14:41:29 +0800

    Modified data structures

--- src/data_structures/Variable.java
+++ src/data_structures/Variable.java
@@ -2,42 +2,12 @@ package data_structures;
 
 public class Variable {
     private int id;
-    private int value;
-    private int decisionLevel;
-    private Variable ancestor;
 
     public Variable(int id) {
         this.id = id;
-        this.value = -1;
-        this.decisionLevel = -1;
-        this.ancestor = null;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public void setDecisionLevel(int decisionLevel) {
-        this.decisionLevel = decisionLevel;
-    }
-
-    public void setAncestor(Variable ancestor) {
-        this.ancestor = ancestor;
     }
 
     public int getId() {
         return this.id;
-    }
-    
-    public int getValue() {
-        return this.value;
-    }
-
-    public int getDecisionLevel() {
-        return this.decisionLevel;
-    }
-
-    public Variable getAncestor() {
-        return this.ancestor;
     }
 }
