eaae1b9ed758b0c4fb21c3782da1efae5e0ac23a
lowjiansheng <lowjiansheng@hotmail.sg>
2019-04-21 16:16:36 +0800

    remove variable changes

--- src/main/java/data_structures/Variable.java
+++ src/main/java/data_structures/Variable.java
@@ -2,32 +2,16 @@ package data_structures;
 
 public class Variable {
     private int id;
-    // For bayesian network only
-    private String bayesianId;
-    // TODO: probably have a better name for this
-    private boolean happens;
 
     public Variable(int id) {
         this.id = id;
     }
 
 
-    public Variable(String id, boolean happens) {
-        this.bayesianId = id;
-        this.happens = happens;
-    }
-
     public int getId() {
         return this.id;
     }
 
-    public String getBayesianId() {
-        return this.bayesianId;
-    }
-
-    public boolean isHappen() {
-        return this.happens;
-    }
 
     @Override
     public int hashCode(){
@@ -44,11 +28,7 @@ public class Variable {
         if (getClass() != obj.getClass())
             return false;
         Variable other = (Variable) obj;
-        if (other.getBayesianId() == null && this.getBayesianId() != null ||
-            other.getBayesianId() != null && this.getBayesianId() == null) {
-            return false;
-        }
-        if (other.getBayesianId() == this.getBayesianId() && other.isHappen() == this.isHappen()) {
+        if (other.getId() == this.getId()) {
             return true;
         }
         return false;
