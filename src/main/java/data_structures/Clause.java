9fcf28e28600ee3dca3c1b8f2e36ec37179b5c44
szenius <ting.szeying@gmail.com>
2019-04-21 13:14:42 +0800

    very complicated find unit literal algo

--- src/main/java/data_structures/Clause.java
+++ src/main/java/data_structures/Clause.java
@@ -1,8 +1,6 @@
 package data_structures;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class Clause {
     private List<Literal> literals;
@@ -15,22 +13,13 @@ public class Clause {
         return this.literals;
     }
 
-    public boolean evaluatesToFalse(Assignments2 assignments, Variable newlyAssignedVariable, boolean newlyAssignedValue) {
+    public Literal findLiteralByNode(Node node) {
         for (Literal literal : literals) {
-            if (literal.getVariable().equals(newlyAssignedVariable)) {
-                if (literal.isNegated() ^ newlyAssignedValue) {
-                    return false;
-                }
-                continue;
-            }
-            if (!assignments.hasAssignedVariable(literal)) {
-                return false;
-            }
-            if ((literal.isNegated() ^ assignments.getVariableAssignment(literal))) {
-                return false;
+            if (node.getVariable().equals(literal.getVariable())) {
+                return literal;
             }
         }
-        return true;
+        return null;
     }
 
     public Literal getUnitLiteral(Assignments2 assignments, Variable lastAssignedVariable) {
@@ -52,13 +41,45 @@ public class Clause {
             return null;
         }
         if (unitLiteral == null && lastAssignedVariable != null) {
-            for (Literal literal : getLiterals()) {
-                if (literal.getVariable().equals(lastAssignedVariable)) {
-                    return literal;
+            // All variables have been assigned but this clause evaluates to false
+            Set<Node> nodes = assignments.getNodes(this);
+            Map<Integer, List<Node>> nodesByDecisionLevel = new HashMap<>();
+            int maxDecisionLevel = Integer.MIN_VALUE;
+            for (Node node : nodes) {
+                int decisionLevel = node.getDecisionLevel();
+                List<Node> nodesAtDecisionLevel = nodesByDecisionLevel.getOrDefault(decisionLevel, new ArrayList<>());
+                nodesAtDecisionLevel.add(node);
+                nodesByDecisionLevel.putIfAbsent(decisionLevel, nodesAtDecisionLevel);
+                maxDecisionLevel = Math.max(maxDecisionLevel, decisionLevel);
+            }
+            if (nodesByDecisionLevel.size() == 3) {
+                // All literals were assigned at different decision levels. Return the one assigned last.
+                return findLiteralByNode(nodesByDecisionLevel.get(maxDecisionLevel).get(0));
+            }
+            List<Node> candidates = nodesByDecisionLevel.get(maxDecisionLevel);
+            for (Node candidate : candidates) {
+                if (candidate.getOutEdges().isEmpty()) {
+                    // Found a leaf in the implication graph
+                    return findLiteralByNode(candidate);
                 }
             }
+            return findLiteralByNode(findDeepestNode(candidates.get(0), candidates.get(0), candidates));
         }
         return unitLiteral;
+    }
+
+    private Node findDeepestNode(Node currNode, Node lastFoundNode, List<Node> nodes) {
+        if (nodes.contains(currNode)) {
+            lastFoundNode = currNode;
+        }
+        if (currNode.getOutEdges().isEmpty()) {
+            return lastFoundNode;
+        }
+        List<Edge> paths = currNode.getOutEdges();
+        for (Edge path : paths) {
+            findDeepestNode(path.getToNode(), lastFoundNode, nodes);
+        }
+        return lastFoundNode;
     }
 
     public boolean checkVALID(Assignments assignments) {
