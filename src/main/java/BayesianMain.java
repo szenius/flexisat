78c98cc2195cda76ec62b97bf2ddd788a6f05288
lowjiansheng <lowjiansheng@hotmail.sg>
2019-04-24 13:58:56 +0800

    allow arguments for different encoder

--- src/main/java/BayesianMain.java
+++ src/main/java/BayesianMain.java
@@ -9,9 +9,14 @@ import java.util.Map;
 
 public class BayesianMain {
 
+    public enum ENCODER {
+        CLASS_ENCODER, ALT_ENCODER
+    }
+
     public static void main(String[] args) {
         String fileName = args[0];
         String evidenceFileName = args[1];
+        ENCODER encoderUsed = ENCODER.valueOf(args[2]);
 
         UAIParser parser = new UAIParser();
         parser.parse(fileName);
@@ -19,10 +24,27 @@ public class BayesianMain {
         List<BayesianClique> cliques = parser.getCliques();
         parser.parseEvidence(evidenceFileName);
         Map<Integer, Boolean> queryVariables = parser.getQueryValues();
+        // Instantiate either the ClassBayesianEncoder or the AltBayesianEncoder
+        BayesianEncoder encoder;
+        switch (encoderUsed) {
+            case CLASS_ENCODER:
+                System.out.println("Using the encoding scheme taught in class...");
+                encoder = new ClassBayesianEncoder();
+                break;
+            case ALT_ENCODER:
+                System.out.println("Using an alternative encoding scheme...");
+                encoder = new AltBayesianEncoder();
+                break;
+            default:
+                System.out.println("Please input a legal encoder type.");
+                System.exit(1);
+        }
+
         BayesianEncoder networkEncoder = new ClassBayesianEncoder();
 
         networkEncoder.encodeBayesianQueryIntoCNF(parser.getNumVariables(),
                 cliques, parser.getQueryValues());
+        System.out.println("Finished encoding network in CNF.");
     }
 
 }
