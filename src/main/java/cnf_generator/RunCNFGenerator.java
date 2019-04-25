b64cbbd364b01598b216174ea606f492442962b6
szenius <ting.szeying@gmail.com>
2019-04-25 21:36:45 +0800

    Replaced test set ya

--- src/main/java/cnf_generator/RunCNFGenerator.java
+++ src/main/java/cnf_generator/RunCNFGenerator.java
@@ -2,8 +2,8 @@ package cnf_generator;
 
 public class RunCNFGenerator {
     public static void main(String[] args) {
-        int[] testNumClauses = new int[]{250, 300, 350, 400, 450, 500};
-        int[] testNumVariables = new int[]{100, 200, 300, 400, 500};
+        int[] testNumClauses = new int[]{200, 250, 300, 350, 400};
+        int[] testNumVariables = new int[]{20, 50, 100, 300, 500};
 
         for (int numVariables : testNumVariables) {
             for (int numClauses : testNumClauses) {
