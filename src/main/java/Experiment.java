a1a8fc911fb4b8c1646c998531f3ab84c8ede926
szenius <ting.szeying@gmail.com>
2019-04-25 13:17:32 +0800

    Turned off debug logs, added warn logs for experiment script

--- src/main/java/Experiment.java
+++ src/main/java/Experiment.java
@@ -1,6 +1,8 @@
 import branch_pickers.BranchPickerType;
 import conflict_analysers.ConflictAnalyserType;
 import data_structures.SolverResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -12,6 +14,8 @@ import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 public class Experiment {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Experiment.class);
+
     private static final String TEST_DIRECTORY = "input/test/";
     private static final BranchPickerType DEFAULT_BRANCH_PICKER_TYPE = BranchPickerType.CHAFF;
     private static final ConflictAnalyserType DEFAULT_CONFLICT_ANALYSER_TYPE = ConflictAnalyserType.SINGLE_UIP;
@@ -24,14 +28,20 @@ public class Experiment {
         for (File testFile : testFiles) {
             // Test different branching heuristics
             for (BranchPickerType branchPickerType : BranchPickerType.values()) {
+                LOGGER.warn("Running {} against {} and {}", testFile.getName(), branchPickerType.getType(), DEFAULT_CONFLICT_ANALYSER_TYPE.getType());
                 SolverResult result = Main.run(new String[]{testFile.getAbsolutePath(), branchPickerType.getType(), DEFAULT_CONFLICT_ANALYSER_TYPE.getType()});
-                results.add(buildResultString(testFile.getName(), result, branchPickerType, DEFAULT_CONFLICT_ANALYSER_TYPE));
+                String resultString = buildResultString(testFile.getName(), result, branchPickerType, DEFAULT_CONFLICT_ANALYSER_TYPE);
+                LOGGER.warn("COMPLETED: {}", resultString);
+                results.add(resultString);
             }
 
             // Test different conflict analyser
             for (ConflictAnalyserType conflictAnalyserType : ConflictAnalyserType.values()) {
+                LOGGER.warn("Running {} against {} and {}", testFile.getName(), DEFAULT_BRANCH_PICKER_TYPE.getType(), conflictAnalyserType.getType());
                 SolverResult result = Main.run(new String[]{testFile.getAbsolutePath(), DEFAULT_BRANCH_PICKER_TYPE.getType(), conflictAnalyserType.getType()});
-                results.add(buildResultString(testFile.getName(), result, DEFAULT_BRANCH_PICKER_TYPE, conflictAnalyserType));
+                String resultString = buildResultString(testFile.getName(), result, DEFAULT_BRANCH_PICKER_TYPE, conflictAnalyserType);
+                LOGGER.warn("COMPLETED: {}", resultString);
+                results.add(resultString);
             }
         }
 
