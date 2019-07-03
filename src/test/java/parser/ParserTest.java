703ff2d306500d9f7a03344b2b75224cdb8aac5e
szenius <ting.szeying@gmail.com>
2019-07-03 17:57:17 +0800

    Updated test file path in parser test

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -13,9 +13,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ParserTest {
 
     @Test
-    @DisplayName("Parses a valid cnf.")
+    @DisplayName("Parses a sat cnf.")
     void validParseTest() {
-        String filePath = "input/valid_input1.cnf";
+        String filePath = "input/sat/sat_input1.cnf";
         Parser testParser = new Parser(new String[]{filePath, SEQ.getType(), SINGLE_UIP.getType()});
         Set<Clause> listOfClause = testParser.getClauses().getClauses();
 
